c2eece86ba5089195633a3ff71b19dfb19694b04
savindran <148077194+savindran@users.noreply.github.com>
2024-01-08 05:45:34 +0530

    Update RewardsConverter.java

--- RewardsConverter.java
+++ RewardsConverter.java
@@ -1,20 +1,26 @@
-import java.util.Scanner;
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
 
-public class RewardsConverter {
-    public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Credit Card Rewards Converter!");
-        System.out.println("Please enter a cash value to convert to airline miles: ");
-        var input_value = scanner.nextLine();
-        double cashValue;
-        try {
-            cashValue = Double.parseDouble(input_value);
-        } catch (NumberFormatException exception) {
-            System.out.println("Could not parse input value as a double, exiting");
-            return;
-        }
-        System.out.println("converting $" + input_value + " to miles");
-        var rewardsValue = new RewardValue(cashValue);
-        System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
+    // Constructor accepting cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / 0.0035;
+    }
+
+    // Constructor accepting miles value
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
+    }
+
+    // Getter for cashValue
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Getter for milesValue
+    public double getMilesValue() {
+        return milesValue;
     }
 }
